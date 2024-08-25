f5e6d96a214ceef0b5bf134bb32ccbbe65efcc44
PC <dark.redstar007@gmail.com>
2024-08-25 17:12:09 +0700

    video 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/usser.controller.spec';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
