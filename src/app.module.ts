7582f4949be39ef8bf7155fa2525581011072218
PC <dark.redstar007@gmail.com>
2024-08-26 22:35:07 +0700

    video 18

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://taikhoanrac741:d7WDowD8a5HuYGb7@cluster0.n1tk5.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
