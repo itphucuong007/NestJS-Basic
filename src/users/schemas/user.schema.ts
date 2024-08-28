565293a94e2aedc2bb0c3d93270f5d6a2f5c9ce1
PC <dark.redstar007@gmail.com>
2024-08-28 18:46:18 +0700

    video 21

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phone: number;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  created_at: Date;
+
+  @Prop()
+  updated_at: Date;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
