117ccba1f5e406caa68b7131cded02de4b5fcc2b
PC <dark.redstar007@gmail.com>
2024-08-31 15:36:49 +0700

    video 22.5

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
 import { ConfigService } from '@nestjs/config';
+
+import { ValidationPipe } from '@nestjs/common';
 
 
 async function bootstrap() {
@@ -14,6 +15,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(configService.get<string>('PORT'));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
 
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+
+
 export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty({
+        message: 'chưa nhập email'
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'chưa nhập password'
+    })
     password: string;
     name: string;
     address: string;
