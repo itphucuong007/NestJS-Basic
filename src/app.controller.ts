494a9160076735192eb483484cb5e553e4b51ee6
PC <dark.redstar007@gmail.com>
2024-09-05 10:57:24 +0700

    video 39

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,24 @@
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
-
-
 import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private configService: ConfigService
+    private configService: ConfigService,
+
+    private authService: AuthService
 
     ) { }
   
     @UseGuards(LocalAuthGuard)
     @Post('/login')
     handleLogin(@Request() req) {
-        return req.user;
+        return this.authService.login(req.user);
     }
     
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
 import { UsersService } from 'src/users/users.service';
 
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private usersService: UsersService,
 
-    constructor(private usersService: UsersService) { }
+        private jwtService: JwtService,
+        
+    ) { }
 
+    //ussername/ pass là 2 tham số thư viện passport nó ném về
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOneByUsername(username);
-        
-        // kiểm tra user.username phải tồn tại --> mới validate password
         if (user) {
             const isValid = this.usersService.isValidPassword(pass, user.password);
             if (isValid === true) {
@@ -20,6 +23,16 @@ export class AuthService {
         }
 
         return null;
+    }
+
+    async login(user: any) {
+        const payload = {
+            username: user.email,
+            sub: user._id
+        };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 
 }
