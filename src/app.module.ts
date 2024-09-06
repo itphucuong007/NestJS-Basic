e680d3eb13bb41bff91e78b88bbdf29eb916fb24
PC <dark.redstar007@gmail.com>
2024-09-06 23:55:22 +0700

    video 41

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
 
 
 @Module({
@@ -23,12 +26,17 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     UsersModule,
-
     AuthModule
-    
+
   ],
 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+
+  ],
 })
 export class AppModule { }
