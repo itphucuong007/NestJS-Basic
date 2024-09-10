1182eefe26a0eb053b046d058accf0aa24582bf8
PC <dark.redstar007@gmail.com>
2024-09-11 02:26:37 +0700

    video 56

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
+
+import { IUser } from 'src/users/user.interface';
 
 
 @Injectable()
@@ -15,9 +16,17 @@ export class CompaniesService {
     private companyModel: SoftDeleteModel<CompanyDocument>
   ) { }
 
-  create(createCompanyDto: CreateCompanyDto) {
-    return this.companyModel.create({...createCompanyDto})
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
   }
+
+
 
   findAll() {
     return `This action returns all companies`;
