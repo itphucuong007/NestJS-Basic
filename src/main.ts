4648aa5859b4f069932457b92d7296d43f077e79
PC <dark.redstar007@gmail.com>
2024-09-17 12:34:40 +0700

    Video 61.1

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,25 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-
 import { NestFactory, Reflector } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+import { TransformInterceptor } from './core/transform.interceptor';
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-
   const reflector = app.get(Reflector);
-
   app.useGlobalGuards(new JwtAuthGuard(reflector));
-
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
 
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  
   // config CORS
   app.enableCors({
     "origin": "http://localhost:3000/",
