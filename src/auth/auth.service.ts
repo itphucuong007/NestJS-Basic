7cf3fde24a7d409dc8f6c5b95c049a59c52cbd50
PC <dark.redstar007@gmail.com>
2024-11-24 00:08:36 +0700

    Video 69.1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/users/user.interface';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { ConfigService } from '@nestjs/config';
 import ms from 'ms';
-
 import { Response } from 'express';
 import { log } from 'console';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 
 @Injectable()
@@ -33,7 +33,7 @@ export class AuthService {
         return null;
     }
 
-    async login(user: IUser, response : Response) {
+    async login(user: IUser, response: Response) {
         const { _id, name, email, role } = user;
         const payload = {
             sub: "token login", // Thêm subject
@@ -53,7 +53,7 @@ export class AuthService {
         //set refresh_token as cookies
         response.cookie('refresh_token', refresh_token, {
             httpOnly: true,
-            maxAge: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE"))
+            maxAge: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE")) * 1000
         })
 
 
@@ -85,6 +85,16 @@ export class AuthService {
             createdAt: newUser?.createdAt
         }
 
+    }
+
+    processNewToken = (refreshToken: string) => {
+        try {
+            this.jwtService.verify(refreshToken, {
+                secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET')
+            })
+        } catch (error) {
+            throw new BadRequestException("Refresh token không hợp lệ. Vui lòng login lại")
+        }
     }
 
 }
