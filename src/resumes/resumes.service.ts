51445cec73777b33b0f58b49d004a12d9562ec75
PC <dark.redstar007@gmail.com>
2024-12-10 19:05:24 +0700

    video 82

--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -1,26 +1,151 @@
-import { Injectable } from '@nestjs/common';
-import { CreateResumeDto } from './dto/create-resume.dto';
 import { UpdateResumeDto } from './dto/update-resume.dto';
+import { CreateResumeDto, CreateUserCvDto } from './dto/create-resume.dto';
+import { IUser } from 'src/users/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Resume, ResumeDocument } from './schemas/resume.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
+import mongoose from 'mongoose';
+
+
 
 @Injectable()
 export class ResumesService {
-  create(createResumeDto: CreateResumeDto) {
-    return 'This action adds a new resume';
+
+  constructor(
+    @InjectModel(Resume.name)
+    private resumeModel: SoftDeleteModel<ResumeDocument>
+  ) { }
+
+
+  async create(createUserCvDto: CreateUserCvDto, user: IUser) {
+    const { url, companyId, jobId } = createUserCvDto;
+    const { email, _id } = user;
+
+    const newCV = await this.resumeModel.create({
+      url, companyId, email, jobId,
+      userId: _id,
+      status: "PENDING",
+      createdBy: { _id, email },
+      history: [
+        {
+          status: "PENDING",
+          updatedAt: new Date,
+          updatedBy: {
+            _id: user._id,
+            email: user.email
+          }
+        }
+      ]
+    })
+
+    return {
+      _id: newCV?._id,
+      createdAt: newCV?.createdAt
+    };
+
   }
 
-  findAll() {
-    return `This action returns all resumes`;
+
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.resumeModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.resumeModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hién tai |
+        pageSize: limit, //s6 lugng ban ghi da ldy |
+        pages: totalPages, //tdng s6 trang voi didu kién query
+        total: totalItems // tng s6 phan tir (sé ban ghi) |
+
+      },
+      result //két qua query 
+    }
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} resume`;
+
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('not found resume')
+    }
+    return await this.resumeModel.findById(id);
   }
 
-  update(id: number, updateResumeDto: UpdateResumeDto) {
-    return `This action updates a #${id} resume`;
+  async update(_id: string, status: string, user: IUser) {
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw new BadRequestException('not found resume')
+    }
+
+    const updated = await this.resumeModel.updateOne(
+      { _id },
+
+      {
+        status,
+        updatedBy: {
+          _id: user._id,
+          email: user.email
+        },
+
+        $push: {
+          history: {
+            status: status,
+            updatedAt: new Date,
+            updatedBy: {
+              _id: user._id,
+              email: user.email
+            }
+          }
+        }
+      })
+
+    return updated;
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} resume`;
+  async remove(id: string, user: IUser) {
+    await this.resumeModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      })
+
+    return this.resumeModel.softDelete({
+      _id: id
+    })
+
   }
+
+
+  async findByUsers(user: IUser) {
+    return await this.resumeModel.find({
+      userId: user._id
+    })
+  }
+
+
+
+
 }
+
+
