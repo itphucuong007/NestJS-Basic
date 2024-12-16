c0b1a9ced117064e86237379cfac7562d2d1db38
PC <dark.redstar007@gmail.com>
2024-12-16 17:55:15 +0700

    Video 88

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/user.interface';
+
+
+
 @Controller('roles')
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
+  constructor(private readonly rolesService: RolesService) { }
+
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.rolesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  @ResponseMessage('Create a new role')
+  create(@Body() createRoleDto: CreateRoleDto, @User() user: IUser) {
+    return this.rolesService.create(createRoleDto, user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  @ResponseMessage('Update a role')
+  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto, @User() user: IUser) {
+    return this.rolesService.update(id, updateRoleDto, user);
+  }
+
+  @Get()
+  @ResponseMessage('Fetch roles with paginate')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query("pageSize") limit: string,
+    @Query() qs: string
+  ) {
+    return this.rolesService.findAll(+currentPage, +limit, qs);
+  }
+
+
+  @Get(':id')
+  @ResponseMessage("Fetch a role by id")
+  findOne(@Param('id') id: string) {
+    return this.rolesService.findOne(id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  @ResponseMessage('Delete a role')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.rolesService.remove(id, user);
   }
+
+
+
 }
