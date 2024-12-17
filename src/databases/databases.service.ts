1037d62f8ac55f1d26e0f66f4b5a1cdfe636679a
PC <dark.redstar007@gmail.com>
2024-12-17 23:30:26 +0700

    video 90

---
+++ src/databases/databases.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Permission, PermissionDocument } from 'src/permissions/schemas/permission.schema';
+import { Role, RoleDocument } from 'src/roles/schemas/role.schema';
+import { User, UserDocument } from 'src/users/schemas/user.schema';
+
+
+@Injectable()
+export class DatabasesService {
+
+    constructor(
+        @InjectModel(User.name)
+        private userModel: SoftDeleteModel<UserDocument>,
+
+        @InjectModel(Permission.name)
+        private permissionModel: SoftDeleteModel<PermissionDocument>,
+
+        @InjectModel(Role.name)
+        private roleModel: SoftDeleteModel<RoleDocument>,
+
+    ) { }
+
+
+onModuleInit() {
+    console.log(`The module has been initialized.`);
+}
+
+
+}
